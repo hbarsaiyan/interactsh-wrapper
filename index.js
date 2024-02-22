097631aa14c6ce1cb1acebdc0984f198d2ef4f33
Harsh Barsaiyan <hbarsaiyan@gmail.com>
2024-02-23 02:25:46 +0530

    Add startup text

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 import express from 'express'
 import config from './utils/config.js'
+import figlet from 'figlet'
 import routeHandler from './controllers/handleRoutes.js'
 import cors from 'cors'
+
+figlet('interactsh-wrapper', function (err, data) {
+    if (err) {
+        console.log('Something went wrong...')
+        console.dir(err)
+        return
+    }
+    console.log(data)
+})
 
 const app = express()
 app.use(express.json())
